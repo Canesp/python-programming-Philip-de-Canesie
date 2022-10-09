bfffa81d6d74e9949a206ab0a56d551e7881372b
Canesp <philip.decanesie@gmail.com>
2022-10-09 18:09:50 +0200

    Labb-3 translation and graph plot + point check

--- Labs/Labb-3/geometry_shapes.py
+++ Labs/Labb-3/geometry_shapes.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
+from matplotlib.patches import Circle
 import math
 
 class shapes:
@@ -20,7 +21,7 @@ class shapes:
         
         if not isinstance(value, (int, float)):
             raise TypeError(f"X value need to be a int or float, not -> ({type(value)})")
-        print("x!")
+        
         self._x = value
 
     @property
@@ -32,8 +33,68 @@ class shapes:
         
         if not isinstance(value, (int, float)):
             raise TypeError(f"Y value need to be a int or float, not -> ({type(value)})")
-        print(f"y! {value=}")
+    
         self._y = value
+
+    def plot_size(self):
+
+        if self.__class__.__name__ == "Rektangel":
+
+            x_size = self._x + self._width + 2
+            y_size = self._y + self._height + 2
+            
+            if x_size < y_size:
+                x_size = y_size
+            else:
+                y_size = x_size
+                
+            return (x_size, y_size)
+
+        else:
+
+            x_size = self._x + self.radius + 2
+            y_size = self._y + self.radius + 2
+
+            if x_size < y_size:
+                x_size = y_size
+            else:
+                y_size = x_size
+                
+            return (x_size, y_size)
+    #TODO felhantering 
+    def draw_graph(self):
+
+        fig ,ax = plt.subplots(1,1, figsize=(8,8))
+
+        x_size, y_size = self.plot_size()
+
+        ax.set_xlim((-x_size, x_size))
+        ax.set_ylim((-y_size, y_size))
+        
+        if self.__class__.__name__ == "Rektangel":
+
+            ax.add_patch(Rectangle((self._x, self._y), self._width, self._height, linewidth=2, edgecolor="b",facecolor='none', ))
+
+        elif self.__class__.__name__ == "Cirkel":
+
+            ax.add_patch(Circle((self._x, self._y), self._radius, linewidth=2, edgecolor="b",facecolor='none', ))
+    #TODO felhantering 
+    def translation(self, x: int | float, y: int | float):
+
+        self._x += x
+        self._y += y
+
+        self.draw_graph()
+    #TODO felhantering 
+    def check_point(self, x: int | float, y: int | float):
+
+        if self.__class__.__name__ == "Rektangel":
+            
+            return abs(x) - abs(self._x + (self._width / 2)) < 0 and abs(y) - abs(self._y + (self._height / 2)) < 0
+        
+        else: 
+            
+            return math.sqrt(pow(x - self._x, 2) + pow(y - self._y, 2)) < self._radius
 
     ##--------------------------------------------------------------------##
 
@@ -61,7 +122,34 @@ class Rektangel(shapes):
 
         self.width = width
         self.height = height
-       
+
+    @property
+    def width(self) -> float:
+        return self._width
+    
+    @width.setter
+    def width(self, value):
+        
+        if not isinstance(value, (int, float)):
+            raise TypeError(f"width need to be a int or float, not -> ({type(value)})")
+        elif value <= 0: 
+            raise ValueError(f"width need to be greater then zero, not -> ('{value}')")
+        
+        self._width = value
+    
+    @property
+    def height(self) -> float:
+        return self._height
+    
+    @height.setter
+    def height(self, value):
+        
+        if not isinstance(value, (int, float)):
+            raise TypeError(f"height need to be a int or float, not -> ({type(value)})")
+        elif value <= 0: 
+            raise ValueError(f"height need to be greater then zero, not -> ('{value}')")
+        
+        self._height = value
 
     @property
     def area(self) -> float:
